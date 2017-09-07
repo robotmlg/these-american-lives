6fedfc7420f8e1f684235162a05fe67f42a766f1
Matt Goldman <robotmlg@gmail.com>
2017-09-07 00:09:40 -0400

    DB setup

---
+++ TALReruns/Sources/App/EpisodeRepository.swift
@@ -0,0 +1,10 @@
+//
+//  EpisodeRepository.swift
+//  TALReruns
+//
+//  Created by Matt Goldman on 9/6/17.
+//
+//
+
+import PostgreSQL
+
--- schema.sql
+++ schema.sql
@@ -1,6 +1,3 @@
-
-CREATE SCHEMA IF NOT EXISTS tal;
-
 CREATE TABLE IF NOT EXISTS episodes(
     episode_id INTEGER PRIMARY KEY
   , title VARCHAR(256) NOT NULL
@@ -13,5 +10,20 @@ CREATE TABLE IF NOT EXISTS airings(
     airing_id SERIAL PRIMARY KEY
   , episode_id INTEGER NOT NULL REFERENCES episodes (episode_id)
   , air_date DATE UNIQUE NOT NULL
-);
+);  
 
+COPY episodes FROM '/Volumes/Mac Data/Code/TALReruns/episodes20170902.csv' WITH (FORMAT csv, HEADER TRUE);
+
+COPY airings (episode_id, air_date) FROM '/Volumes/Mac Data/Code/TALReruns/original_airdates.csv' WITH (FORMAT csv, HEADER TRUE);
+
+CREATE OR REPLACE VIEW original_airings AS
+	SELECT e.*, original_air_date FROM episodes e
+	INNER JOIN (
+		SELECT episode_id, MIN(air_date) original_air_date
+		FROM airings a
+		GROUP BY a.episode_id
+	) tbl ON e.episode_id = tbl.episode_id;
+	
+CREATE OR REPLACE VIEW all_airings AS
+	SELECT e.*, a.air_date FROM episodes e
+	INNER JOIN airings a ON e.episode_id = a.episode_id;
